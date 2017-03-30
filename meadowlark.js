3785c2666791239d585fb187d14e644f36cc14e0
Rey Bango <reybango@gmail.com>
2017-03-29 22:24:10 -0400

    npm init and main file created

---
+++ meadowlark.js
@@ -0,0 +1,36 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+  res.type('text/plain');
+  res.send('Meadowlark Travel');
+});
+
+app.get('/about', function(req, res){
+  res.type('text/plain');
+  res.send('About Meadowlark Travel');
+});
+
+// Custom 404 page
+app.use(function(req, res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not found');
+});
+
+// Custom 500 page
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + 
+  app.get('port') + '; Press CTRL-C to terminate.');
+});
+
